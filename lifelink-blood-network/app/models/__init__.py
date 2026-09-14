3dbf2ac7499af11a1388c59f98548cfdcf252a41
shakeel946 <asimshakeel2k@gmail.com>
2026-09-14 23:29:39 +0530

    Delete lifelink-blood-network directory

--- lifelink-blood-network/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.user import User
-from app.models.donor import DonorProfile, DonationHistory
-
-__all__ = ["User", "DonorProfile", "DonationHistory"]
--- lifelink-blood-network/tests/test_users.py
+++
@@ -1,8 +0,0 @@
-from app.core.security import hash_password, verify_password
-
-def test_password_hashing():
-    password = "StrongPass123!"
-    hashed = hash_password(password)
-    assert hashed != password
-    assert verify_password(password, hashed)
-    assert not verify_password("wrong", hashed)
